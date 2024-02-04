2807b919ed240cdf317c97cd24777deaaa9e85d7
haneen898 <haneenmansoor898@gmail.com>
2024-02-04 21:06:29 +0300

    final commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var myName = 'devo' 
+var myAge = 24  
+var IsStudent = true 
+console.log("Hey" ,myName) 
+console.log( "Devo" , myAge) 
+console.log("haneen", IsStudent) 
+ 
+ 
+var number1 = 10 
+var number2 = 5 
+ 
+console.log ("sum" , number1 + number2); 
+console.log ("divition" , 6/2); 
+console.log ( "multip" , 10*2); 
+console.log ("minus" , number1 - number2); 
+ 
+ 
+console.log("type of isstudent" , typeof myName) 
+console.log( "type of myName" , typeof myName) 
+console.log ("type of age is" ,typeof myAge) 
+ 
+ 
+var number3 = 20 
+var number4 = 45 
+ 
+console.log ("3 greater than 4" , number3 > number4) 
+console.log("3 is less than 4", number3 < number4) 
+console.log("equal", number3 == number4) 
+console.log("greater or equal", number3 >= number4) 
+ 
+ 
+var name = 'Haneen'  
+var greeting = 'Hello'  
+var massage = greeting + name  
+console.log (massage)
